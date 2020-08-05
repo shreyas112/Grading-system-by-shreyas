1a5d044018030cb9b418db10377713ccbd734a2e
shreyas112 <69191843+shreyas112@users.noreply.github.com>
2020-08-05 14:40:30 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+
 int main()
 {
     printf("Enter the marks of the student:");
